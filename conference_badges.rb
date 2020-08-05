f335effb16f2580e0bd018d7df08dc1c9cb6f549
daniel campo <campo_daniel94@yahoo.com>
2020-08-05 02:18:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,8 +21,8 @@ def assign_rooms(room_assignments)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees) do |badges|
-    puts badges
+  batch_badge_creator(attendees) do |badge|
+    puts badge
 end
 
 
