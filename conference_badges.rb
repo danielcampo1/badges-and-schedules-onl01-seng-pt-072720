208e3ac17da0b55a47beb6912f5cdd50749d6b69
daniel campo <campo_daniel94@yahoo.com>
2020-08-05 02:26:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,9 +21,8 @@ def assign_rooms(room_assignments)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
-    assign_rooms(attendees).each do |room|
+  batch_badge_creator(attendees).each{ |badge| puts badge}
+    assign_rooms(attendees).each{|room|
       puts rooms
 end
 end
