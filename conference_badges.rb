1f9815495381dd8069459abd14b5f3b724a3e99d
daniel campo <campo_daniel94@yahoo.com>
2020-08-05 01:40:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+require "pry"
+
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    new_array = []
+  attendees.each do |people|
+   new_array.push(badge_maker(people))
+  end
+    new_array
+end
+
+def assign_rooms(room_assignments)
+    room_assignments.each do |room|
+      puts "hello, #{room_assignments}! You'll be asssigned to room #{each_with_index}"
+ binding.pry
+  end
+    
+end
