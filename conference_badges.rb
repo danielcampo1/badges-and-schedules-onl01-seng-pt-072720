3762c395aa219249c313f71546c23fcd9ff90711
daniel campo <campo_daniel94@yahoo.com>
2020-08-05 02:03:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,7 @@ end
 def assign_rooms(room_assignments)
   new_array = []
     room_assignments.each_with_index do |name, i|
-      new_array.push
-      puts "hello, #{room_assignments}! You'll be asssigned to room #{each_with_index}"
+      new_array.push "hello, #{room_assignments}! You'll be asssigned to room #{each_with_index}"
  binding.pry
   end
     
